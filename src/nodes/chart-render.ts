64e7394aa161bcf5703f0861fee957e411fad18e
barrierye <barriery@qq.com>
2020-05-09 02:38:03 +0800

    add line & pie chart

--- src/nodes/chart-render.ts
+++ src/nodes/chart-render.ts
@@ -10,14 +10,10 @@ interface ChartRenderNode extends Node {
     data: Array<number>
 }
 
-interface ValueSpec {
-    category: string
-    amount: number
-}
-
 interface DataSpec {
     name: string
-    values: Array<ValueSpec>
+    values: object
+    transform?: object
 }
 
 interface ScaleSpec {
@@ -44,15 +40,17 @@ interface MarkSpec {
 interface VegaSpec {
     width: number
     height: number
-    padding: number
+    padding?: number
+    autosize?: string
 
+    signals?: object
     data: Array<DataSpec>
     scales: Array<ScaleSpec>
-    axes: Array<AxesSpec>
+    axes?: Array<AxesSpec>
     marks: Array<MarkSpec>
 }
 
-let default_scales = [
+let default_bar_scales = [
     {
         "name": "xscale",
         "type": "band",
@@ -69,12 +67,49 @@ let default_scales = [
     }
 ]
 
-let default_axes = [
+let default_line_scales = [
+    {
+      "name": "x",
+      "type": "point",
+      "range": "width",
+      "domain": {"data": "table", "field": "x"}
+    },
+    {
+      "name": "y",
+      "type": "linear",
+      "range": "height",
+      "nice": true,
+      "zero": true,
+      "domain": {"data": "table", "field": "y"}
+    },
+    {
+      "name": "color",
+      "type": "ordinal",
+      "range": "category",
+      "domain": {"data": "table", "field": "c"}
+    }
+]
+
+let default_pie_scales = [
+    {
+      "name": "color",
+      "type": "ordinal",
+      "domain": {"data": "table", "field": "id"},
+      "range": {"scheme": "category20"}
+    }
+]
+
+let default_bar_axes = [
     { "orient": "bottom", "scale": "xscale" },
     { "orient": "left", "scale": "yscale" }
 ]
 
-let default_marks = [
+let default_line_axes = [
+    {"orient": "bottom", "scale": "x"},
+    {"orient": "left", "scale": "y"}
+]
+
+let default_bar_marks = [
     {
       "type": "rect",
       "from": {"data":"table"},
@@ -99,27 +134,151 @@ let default_marks = [
     }
 ]
 
-function genJson(node: ChartRenderNode) {
-    let values = Array<ValueSpec>();
-    for (let i = 0; i < node.data.length; ++i) {
-        values.push({"category":i.toString(), "amount": node.data[i]});
-    }
-    let data: DataSpec = {
-        name: 'table',
-        values: values
-    };
+let default_line_marks = [
+  {
+    "type": "group",
+      "from": {
+        "facet": {
+          "name": "series",
+          "data": "table",
+          "groupby": "c"
+        }
+      },
+      "marks": [
+        {
+          "type": "line",
+          "from": {"data": "series"},
+          "encode": {
+            "enter": {
+              "x": {"scale": "x", "field": "x"},
+              "y": {"scale": "y", "field": "y"},
+              "stroke": {"scale": "color", "field": "c"},
+              "strokeWidth": {"value": 2}
+            }
+          }
+        }
+      ]
+   }
+]
 
-    let spec: VegaSpec = {
-        width: 400,
-        height: 200,
-        padding: 5, 
-        data: [data],
-        scales: default_scales,
-        axes: default_axes,
-        marks: default_marks
-    };
-   
-    return spec
+let default_pie_marks = [
+    {
+      "type": "arc",
+      "from": {"data": "table"},
+      "encode": {
+        "enter": {
+          "fill": {"scale": "color", "field": "id"},
+          "x": {"signal": "width / 2"},
+          "y": {"signal": "height / 2"}
+        },
+        "update": {
+          "startAngle": {"field": "startAngle"},
+          "endAngle": {"field": "endAngle"},
+          "padAngle": {"signal": "padAngle"},
+          "innerRadius": {"signal": "innerRadius"},
+          "outerRadius": {"signal": "width / 2"},
+          "cornerRadius": {"signal": "cornerRadius"}
+        }
+      }
+    }
+]
+
+let default_scales: {[key: string]: any[]} = {'Bar chart': default_bar_scales,
+                                              'Line chart': default_line_scales,
+                                              'Pie chart': default_pie_scales}
+
+let default_axes: {[key: string]: any[]} = {'Bar chart': default_bar_axes,
+                                            'Line chart': default_line_axes}
+
+let default_marks: {[key: string]: any[]} = {'Bar chart': default_bar_marks,
+                                             'Line chart': default_line_marks,
+                                             'Pie chart': default_pie_marks}
+
+function genJson(node: ChartRenderNode) {
+    let type = node.chartType
+    if (type == 'Bar chart' || type == 'Line chart') {
+        let values = Array<object>();
+        if (type == 'Bar chart') {
+            for (let i = 0; i < node.data.length; ++i) {
+                values.push({"category":i.toString(), "amount": node.data[i]});
+            }
+        } else if (type == 'Line chart') {
+            for (let i = 0; i < node.data.length; ++i) {
+                values.push({"x": i.toString(), "y": node.data[i], "c": 0});
+            }
+        }
+    
+        let data: DataSpec = {
+            name: 'table',
+            values: values
+        };
+
+        let spec: VegaSpec = {
+            width: 400,
+            height: 200,
+            padding: 5, 
+            data: [data],
+            scales: default_scales[type],
+            axes: default_axes[type],
+            marks: default_marks[type]
+        };
+        return spec;
+    } else if (type == 'Pie chart') {
+        let values = Array<object>();
+        for (let i = 0; i < node.data.length; ++i) {
+            values.push({"id": i.toString(), "field": node.data[i]});
+        }
+
+        let data: DataSpec = {
+            name: 'table',
+            values: values,
+            transform: [
+              {
+                type: "pie",
+                field: "field",
+                startAngle: {"signal": "startAngle"},
+                endAngle: {"signal": "endAngle"},
+                sort: {"signal": "sort"}
+              }
+            ]
+        };
+
+        let spec: VegaSpec = {
+            width: 200,
+            height: 200,
+            autosize: "none",
+            signals: [
+              {
+                name: "startAngle", value: 0,
+                bind: {input: "range", min: 0, max: 6.29, step: 0.01}
+              },
+              {
+                name: "endAngle", value: 6.29,
+                bind: {input: "range", min: 0, max: 6.29, step: 0.01}
+              },
+              {
+                name: "padAngle", value: 0,
+                bind: {input: "range", min: 0, max: 0.1}
+              },
+              {
+                name: "innerRadius", value: 0,
+                bind: {input: "range", min: 0, max: 90, step: 1}
+              },
+              {
+                name: "cornerRadius", value: 0,
+                bind: {input: "range", min: 0, max: 10, step: 0.5}
+              },
+              {
+                name: "sort", value: false,
+                bind: {input: "checkbox"}
+              }
+            ],
+            data: [data],
+            scales: default_scales[type],
+            marks: default_marks[type]
+        }
+        return spec;
+    }
 }
 
 export = (RED: Red) => {
