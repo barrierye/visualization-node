99e42d7b7c1519fe2698d2fe41dd6e3045d9814c
barrierye <barriery@qq.com>
2020-05-10 15:14:48 +0800

    update code

--- src/chart-render.js
+++ src/chart-render.js
@@ -246,7 +246,7 @@ module.exports = (RED) => {
                 .then(function (canvas) {
                 var imgBase64 = canvas.toDataURL("image/jpeg");
                 var imgData = imgBase64.replace(/^data:image\/\w+;base64,/, "");
-                var stream = canvas.createPNGStream();
+                // var stream = canvas.createPNGStream();
                 // const out = fs.createWriteStream("test.png");
                 // stream.pipe(out);
                 // console.log(imgBase64);
